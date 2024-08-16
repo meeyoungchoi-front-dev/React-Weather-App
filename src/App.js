9b46eb0671efa27ee7bc92bd40e73ae8daa2523f
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-16 22:10:36 +0900

    현재 위치 정보 가져오기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      console.log("현재 위치: ", lat, " " , long);
+    });
+
+  }
+  useEffect(() => {
+    getCurrentLocation()
+
+  }, [])
   return (
     <div>
       hi it's weather app
