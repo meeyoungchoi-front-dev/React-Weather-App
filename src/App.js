65518d12c1b7c41992845e1acbb7bd79a62d8d7e
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-17 11:25:47 +0900

    openweathermap api를 활용하여 현재 위치 기반 날씨 데이터를 콘솔에 찍는 코드 수정하여 결과 확인

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import WeatherBox from './component/WeatherBox';
+import WeatherButton from './component/WeatherButton';
 
 function App() {
   let lat = 0;
-  let lon = 0;
-  const getCurrentLocation = useCallback(() => {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-                lat = position.coords.latitude;
-                lon = position.coords.longitude;
-                console.log("현재 위치: ", lat, " ", lon);
-                getWeatherByCurrentLocation(lat, lon);
-            },
-            (error) => {
-                console.error("위치 정보를 가져오지 못했습니다: ", error.message);
-            }
-        );
-    } else {
-        console.error("이 브라우저에서는 Geolocation이 지원되지 않습니다.");
-    }
-}, []); // 빈 배열로 설정하여 처음 한 번만 함수가 생성되도록 함.useEffect(() => {
+  let lon = 0; 
 
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          lat = position.coords.latitude;
+          lon = position.coords.longitude;
+          console.log("현재 위치: ", lat, " ", lon);
+          getWeatherByCurrentLocation(lat, lon);
+        },
+        (error) => {
+          console.error("위치 정보를 가져오지 못했습니다: ", error.message);
+        }
+      );
+    } else {
+      console.error("이 브라우저에서는 Geolocation이 지원되지 않습니다.");
+    }
+  };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
-    let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=debf57bfca72919d88e06dacb4bf18c7`
-    let response = await fetch(url)
-    let data = await response.json();
-    console.log("data", data);
-  } 
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=debf57bfca72919d88e06dacb4bf18c7`;
+    try {
+      let response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      let data = await response.json();  // json() 메서드 호출
+      console.log("Weather Data:", data);  // JSON 응답을 콘솔에 출력
+    } catch (error) {
+      console.error("데이터를 가져오는 중 오류 발생:", error);
+    }
+  };
+
 
   useEffect(() => {
     getCurrentLocation();
-  }, [lat, lon]);
+  }, []);
 
   return (
     <div>
-      hi weather app
+      <WeatherBox/>
+      <WeatherButton/>
     </div>
   );
 }
---
+++ src/component/WeatherButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WeatherButton = () => {
+  return (
+    <div>
+      <button>현재위치</button>
+    </div>
+  )
+}
+
+export default WeatherButton
