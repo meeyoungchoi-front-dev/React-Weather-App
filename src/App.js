96cc24fc18a0e7de9d4d730abf8b47a6a88efa70
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-17 11:54:05 +0900

    getCurrentLocation 에러 수정

--- src/App.js
+++ src/App.js
@@ -4,26 +4,16 @@ import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
 
 function App() {
-  let lat = 0;
-  let lon = 0; 
 
-  const getCurrentLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          lat = position.coords.latitude;
-          lon = position.coords.longitude;
-          console.log("현재 위치: ", lat, " ", lon);
-          getWeatherByCurrentLocation(lat, lon);
-        },
-        (error) => {
-          console.error("위치 정보를 가져오지 못했습니다: ", error.message);
-        }
-      );
-    } else {
-      console.error("이 브라우저에서는 Geolocation이 지원되지 않습니다.");
-    }
-  };
+ const getCurrentLocation = () => {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+     let lat = position.coords.latitude;
+     let lon = position.coords.longitude;
+      console.log("현재 위치: ", lat, " ", lon);
+      getWeatherByCurrentLocation(lat, lon);
+    });
+ }
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=debf57bfca72919d88e06dacb4bf18c7`;
