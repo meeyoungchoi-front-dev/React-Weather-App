2bec35a8dc0564f735f749929c1cf7889fd720bf
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-18 13:33:17 +0900

    지역 버튼 클릭시 클릭된 상태 유지

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-const WeatherButton = ({cities, setCity}) => {
+const WeatherButton = ({ cities, setCity, selectedCity }) => {
   console.log("cities?", cities);
 
   return (
     <div>
-       <Button className="mt-5 me-1" variant="warning" onClick={() => setCity('')}>Current Location</Button>
+       {/* 현재 위치 버튼 */}
+       <Button 
+         className="mt-5 me-1" 
+         variant={selectedCity === null ? "success" : "warning"} 
+         onClick={() => setCity(null)}>Current Location</Button>
+
+       {/* 도시 리스트에 따라 버튼 생성 */}
        {cities.map((item, index) => (
-        <Button className="mt-5 me-1" variant="warning" key={index} onClick={() => setCity(item)}>{item}</Button>
+         <Button 
+           className="mt-5 me-1" 
+           variant={selectedCity === item ? "success" : "warning"} 
+           key={index} 
+           onClick={() => setCity(item)}>
+           {item}
+         </Button>
        ))}
     </div>
   )
 }
 
-export default WeatherButton
+export default WeatherButton;
