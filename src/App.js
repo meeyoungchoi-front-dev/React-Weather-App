be6d70f2b982b3a1b3038ce3c223d6d8f92b4cab
meeyoungchoi-front-dev <choimeeyoung94@gmail.com>
2024-08-18 23:09:06 +0900

    current location 및 지역  버튼 클릭 상태 활성화

--- src/App.js
+++ src/App.js
@@ -8,21 +8,67 @@ import ClipLoader from "react-spinners/ClipLoader";
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
-  const cities  = ['paris', 'new york', 'tokyo', 'seoul', 'incheon'];
+  const cities  = ['paris', 'new york', 'los angeles', 'tokyo', 'seoul', 'incheon', 'busan'];
   let [loading, setLoading] = useState(false);
+  const [buttonActive, setButtonActive] = useState('current');
   let [color] = useState("#ff0000");
 
- const getCurrentLocation = useCallback(() => {
-  navigator.geolocation.getCurrentPosition((position)=>{
-    let lat = position.coords.latitude
-    let lon = position.coords.longitude
-    getWeatherByCurrentLocation(lat, lon)});
-}, []);
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+//  const getCurrentLocation = useCallback(() => {
+//   navigator.geolocation.getCurrentPosition((position)=>{
+//     let lat = position.coords.latitude
+//     let lon = position.coords.longitude
+//     setButtonActive(true);
+//     getWeatherByCurrentLocation(lat, lon)});
+// }, []);
+
+const getCurrentLocation = async () => {
+  if (!navigator.geolocation) {
+    alert("위치 서비스를 지원하지 않습니다.");
+    return;
+  }
+  setLoading(true);
+  setButtonActive("Current Location");
+  navigator.geolocation.getCurrentPosition(
+    async (position) => {
+      const { latitude: lat, longitude: lon } = position.coords;
+      await getWeatherByCurrentLocation(lat, lon);
+    },
+    (error) => {
+      alert("위치 정보를 가져올 수 없습니다. 다시 시도해주세요.");
+      setLoading(false);
+    }
+  );
+};
+
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=debf57bfca72919d88e06dacb4bf18c7&units=metric`;
     try {
       setLoading(true);
+      setCity("current");
+      let response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      let data = await response.json();  // json() 메서드 호출
+      console.log("Weather Data:", data);  // JSON 응답을 콘솔에 출력
+      setWeather(data);
+      setLoading(false); 
+    } catch (error) {
+      console.error("데이터를 가져오는 중 오류 발생:", error);
+    }
+  };
+
+  const getWeatherByCity = async (city) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=debf57bfca72919d88e06dacb4bf18c7&units=metric`;
+    try {
+      setLoading(true);
+      setButtonActive(city);
+      setCity(city);
       let response = await fetch(url);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -36,32 +82,6 @@ function App() {
     }
   };
 
-  const getWeatherByCity = async () => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=debf57bfca72919d88e06dacb4bf18c7&units=metric`;
-    try {
-      setLoading(true);
-      let response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      let data = await response.json();  // json() 메서드 호출
-      console.log("Weather Data:", data);  // JSON 응답을 콘솔에 출력
-      setWeather(data);
-      setLoading(false);
-    } catch (error) {
-      console.error("데이터를 가져오는 중 오류 발생:", error);
-    }
-  }
-
-  useEffect(() => {
-    if (city === "") {
-      getCurrentLocation();
-    } else {
-      getWeatherByCity();
-    }
-  }, [getCurrentLocation, city]);
-
-  
   return (
     <div>
       {loading? <div className="container">
@@ -75,7 +95,7 @@ function App() {
                 </div>  
       :  <div className='container'>
             <WeatherBox weather={weather}/>
-            <WeatherButton cities={cities} setCity={setCity}/>
+            <WeatherButton cities={cities} setCity={getWeatherByCity} getCurrentLocation={getCurrentLocation} buttonActive={buttonActive} setButtonActive={setButtonActive}/>
           </div>}
     </div>
   );
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
+import "../WeatherButton.css";
 
-const WeatherButton = ({ cities, setCity, selectedCity }) => {
-  console.log("cities?", cities);
+const WeatherButton = ({ cities, setCity, getCurrentLocation, buttonActive, setButtonActive }) => {
+
+  // const handleButtonClick = (city) => {
+  //   if (city === "current") {
+  //     setCity("current");
+  //     setButtonActive("current");
+  //   } else {
+  //     setCity(city);
+  //     setButtonActive(city); // 버튼 클릭 시 버튼의 활성화 상태를 설정
+  //   }
+  // };
 
   return (
-    <div>
-       {/* 현재 위치 버튼 */}
-       <Button 
-         className="mt-5 me-1" 
-         variant={selectedCity === "" ? "success" : "warning"} 
-         onClick={() => setCity("")}>Current Location</Button>
+    <div className='buttons'>
+      {/* 현재 위치 버튼 */}
+      <Button 
+        className={`button ${buttonActive === "current" ? "active" : ""}`} 
+        onClick={() => { getCurrentLocation(); setButtonActive("current"); }}>
+        Current Location
+      </Button>
 
-       {/* 도시 리스트에 따라 버튼 생성 */}
-       {cities.map((item, index) => (
-         <Button 
-           className="mt-5 me-1" 
-           variant={selectedCity === item ? "success" : "warning"} 
-           key={index} 
-           onClick={() => setCity(item)}>
-           {item}
-         </Button>
-       ))}
+      {/* 도시 리스트에 따라 버튼 생성 */}
+      {cities.map((item, index) => (
+        <Button 
+          className={`button ${buttonActive === item ? "active" : ""}`} 
+          key={index} 
+          onClick={() => setCity(item)}>
+          {item}
+        </Button>
+      ))}
     </div>
-  )
+  );
 }
 
 export default WeatherButton;
